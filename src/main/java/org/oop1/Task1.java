0cd5e29c560800d44834e20df4730efdce2b615c
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-01-20 11:47:48 +0100

    Task-1--commit_04

---
+++ src/main/java/org/oop1/Task1.java
@@ -0,0 +1,27 @@
+package org.oop1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        seat[1].reserveSeat(true);
+        seat[3].reserveSeat(true);
+        seat[4].reserveSeat(true);
+        seat[5].reserveSeat(true);
+
+        seat[4].reserveSeat(false);
+
+        for (CinemaSeat seats: seat) {
+            System.out.println(seats + " is available: " + seats.isSeatAvailable());
+        }
+    }
+
+    private static CinemaSeat[] seat = {
+            new CinemaSeat(8, "V"),
+            new CinemaSeat(9, "V"),
+            new CinemaSeat(15, "V"),
+            new CinemaSeat(34, "VII"),
+            new CinemaSeat(18, "IX"),
+            new CinemaSeat(10, "XII"),
+            new CinemaSeat(10, "X"),
+    };
+}
